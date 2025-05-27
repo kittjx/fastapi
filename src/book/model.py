1783d396e1e28762ac59ac55f68fdbf660c26e3f
kittjx <kittjx@hotmail.com>
2025-05-27 15:27:15 +0800

    timestamp

--- src/book/model.py
+++ src/book/model.py
@@ -1,7 +1,19 @@
-from sqlmodel import SQLModel, Field
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import Column, DateTime, SQLModel, Field, func
+
 
 class Book(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title: str = Field(min_length=3, max_length=255)
     author: str = Field(min_length=3, max_length=255)
     year: int = Field(ge=1000, le=2025)
+
+    create_at: Optional[datetime] = Field(
+        sa_column=Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    )
+    update_at: Optional[datetime] = Field(
+        sa_column=Column(DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
+    )
+
