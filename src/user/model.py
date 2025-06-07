f8af2c0692ade9fc03af2525ff08a25ed7989275
kittjx <kittjx@hotmail.com>
2025-06-07 16:57:42 +0800

    add role

--- src/user/model.py
+++ src/user/model.py
@@ -1,6 +1,15 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
+from typing import Optional, List
+from sqlmodel import SQLModel, Field, Relationship
 
+
+class UserRoleLink(SQLModel, table=True):
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id", primary_key=True)
+    role_id: Optional[int] = Field(default=None, foreign_key="role.id", primary_key=True)
+
+class Role(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(unique=True, max_length=64)
+    users: List["User"] = Relationship(back_populates="roles", link_model=UserRoleLink)
 
 class UserBase(SQLModel):
     username: str = Field(unique=True, max_length=128)
@@ -10,6 +19,7 @@ class UserBase(SQLModel):
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     passwd_hash: str = Field(max_length=255, exclude=True)
+    roles: List["Role"] = Relationship(back_populates="users", link_model=UserRoleLink)
 
 class UserCreate(UserBase):
     password: str = Field(max_length=255)
